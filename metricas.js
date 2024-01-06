bb54cef33e7f60193790f838a84f43b45f842438
c.mirkin <ciromirkin@gmail.com>
2024-01-06 13:25:32 -0300

    creacion de arreglo con la informacion de cada apunte

--- metricas.js
+++ metricas.js
@@ -3,13 +3,52 @@ const fs = require('fs');
 // Run file with node metricas.js
 // apunte = note
 
-const RELATIVE_PATH_OF_NOTES = 'apuntes/'
+const RELATIVE_PATH_OF_NOTES_FOLDER = 'apuntes/'
 
-fs.readdir(RELATIVE_PATH_OF_NOTES, (err, listOfNoteFiles) => {
+fs.readdir(RELATIVE_PATH_OF_NOTES_FOLDER, (err, listOfNoteFiles) => {
     if (err) {
         console.error('Error al leer directorio ', err)
         return;
     }
 
-    console.table(listOfNoteFiles)
+    console.table(listOfNoteFiles);
+
+    const notes = []
+    listOfNoteFiles.forEach((nameOfNoteFile, index) => {
+        const RELATIVE_PATH_OF_NOTE_FILE = `${RELATIVE_PATH_OF_NOTES_FOLDER}${nameOfNoteFile}`;
+
+        fs.readFile(RELATIVE_PATH_OF_NOTE_FILE, 'utf-8', (err, noteContent) => {
+            if (err) {
+                console.error('Error al leer apunte (note) ', err);
+            } else {
+                const noteContentPerLine = noteContent.split(/\r\n|\n/);
+
+                let noteTitle = noteContentPerLine[0]
+                noteTitle = [...noteTitle]
+                noteTitle.shift()
+                noteTitle = noteTitle.join('').trim()
+
+                let formattedDate = noteContentPerLine[1]
+                formattedDate = formattedDate.split('_').join('/')
+
+                let noteYear = noteContentPerLine[1].split('_').at(-1)
+
+                console.log(noteYear)
+
+                notes.push({
+                    title: noteTitle,
+                    formattedDate,
+                    year: noteYear,
+                    contentPerLine: noteContentPerLine,
+                    content: noteContent,
+                    RELATIVE_PATH: RELATIVE_PATH_OF_NOTE_FILE
+                })
+
+                const isThisTheLastNote = (listOfNoteFiles.length - 1) === index;
+                if (isThisTheLastNote) {
+                    // Now the whole notes are in notes array
+                }
+            }
+        })
+    })
 });
